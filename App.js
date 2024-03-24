5769b4268044f05c128c7f70efa1994934c16966
Saurabh Singh <164728355+saurabh-singh-9090@users.noreply.github.com>
2024-03-24 17:39:52 +0530

    demo

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ const heading = React.createElement(
 console.log(heading) //heading is a React element in form of an object
  
 // Every rendering in React will happen inside root node
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")); 
 
 // While rendering on the browser, the React element object gets converted into html element
 root.render(heading);
